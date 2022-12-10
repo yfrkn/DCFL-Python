3e0a175bd7aa1cc1513e514b07951ba7f7879cd4
Furkan <113787132+yfrkn@users.noreply.github.com>
2022-12-10 16:54:50 +0300

    Create Ödev-2.py

---
+++ Ödev-2.py
@@ -0,0 +1,19 @@
+# Ödev: okulumuzdaki öğretmenlerin, isimleri, branşları, girdikleri sınıfları; her bir öğretmenin bu verilerini bir listeye ekleyelim.
+# Sonrasında bütün öğretmenleri de daha büyük bir listeye ekleyelim.
+# bu listenin kaç elemanlı olduğunu ekrana yazdıralım
+# öğretmen isimlerine göre alfabetik olarak ekrana yazdıralım
+# öğretmenin isimleri yazarak onunla ilgili verilerin olduğu listeyi ekrana yazdıralım
+ogretmenler = []
+isimler = []
+while True:
+    isim = input("Öğretmenin İsmi : ")
+    brans = input("Öğretmenin Branşı : ")
+    sinif = input("Öğretmenin Girdiği Sınıflar : ")
+    ogretmen = [isim, brans, sinif]
+    ogretmenler.append(ogretmen)
+    ogretmenler.sort()
+    isimler.append(isim)
+    isimler.sort()
+    print("Öğretmen Sayısı : ", len(ogretmenler))
+    print("Öğretmenlerin İsimleri : ", isimler)
+    print("Öğretmenlerin Verileri : ", ogretmenler)

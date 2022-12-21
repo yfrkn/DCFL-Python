1a51bacbea410f7ce40ee6a43690276ce17b437c
Furkan <113787132+yfrkn@users.noreply.github.com>
2022-12-21 21:03:32 +0300

    Create Ödev-4.py

---
+++ Ödev-4.py
@@ -0,0 +1,23 @@
+#                   5. Hafta Ödevi-1
+print("Aldığınız Nota Göre Notunuzu Harflerle İfade Eden Program")
+sinavnotu = int(input("Sınavdan Aldığınız Notu Girin : "))
+if 90 <= sinavnotu <= 100:
+    print("Sınavdan AA Aldınız. Tebrikler!")
+elif 85 <= sinavnotu <= 89:
+    print("Sınavdan BA Aldınız.")
+elif 80 <= sinavnotu <= 84:
+    print("Sınavdan BB Aldınız.")
+elif 75 <= sinavnotu <= 79:
+    print("Sınavdan CB Aldınız.")
+elif 70 <= sinavnotu <= 74:
+    print("Sınavdan CC Aldınız.")
+elif 65 <= sinavnotu <= 69:
+    print("Sınavdan DC Aldınız.")
+elif 60 <= sinavnotu <= 64:
+    print("Sınavdan DD Aldınız.")
+elif 50 <= sinavnotu <= 59:
+    print("Sınavdan FD Aldınız.")
+elif 0 <= sinavnotu <= 49:
+    print("Sınavdan FF Aldınız.")
+else:
+    print("Hatalı Not Girdiniz.")

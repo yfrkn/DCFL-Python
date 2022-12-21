27df0fc66455ac65c57a0e05169c163883f74804
Furkan <113787132+yfrkn@users.noreply.github.com>
2022-12-21 20:38:20 +0300

    Update Ödev-3.py

--- Ödev-3.py
+++ Ödev-3.py
@@ -7,22 +7,15 @@ Toplama İşlemi - 1
 Bölme İşlemi   - 4
 """)
 islem = input("İşlem Numarası : ")
-
+s1 = int(input("1. Sayı = "))
+s2 = int(input("2. Sayı = "))
 if islem == "1":
-    s1 = int(input("1. Sayı = "))
-    s2 = int(input("2. Sayı = "))
     print(s1, "+", s2, "=", s1+s2)
 elif islem == "2":
-    s1 = int(input("1. Sayı = "))
-    s2 = int(input("2. Sayı = "))
     print(s1, "-", s2, "=", s1-s2)
 elif islem == "3":
-    s1 = int(input("1. Sayı = "))
-    s2 = int(input("2. Sayı = "))
     print(s1, "*", s2, "=", s1*s2)
 elif islem == "4":
-    s1 = int(input("1. Sayı = "))
-    s2 = int(input("2. Sayı = "))
     print(s1, "/", s2, "=", s1/s2)
 else:
     print("Hatalı işlem girdiniz.")

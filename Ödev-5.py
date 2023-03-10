0534696feaf25dc75372d393ed88616271ba861c
Furkan <113787132+yfrkn@users.noreply.github.com>
2023-03-10 18:44:39 +0300

    Update Ödev-5.py

--- Ödev-5.py
+++ Ödev-5.py
@@ -28,3 +28,7 @@ else:
 #                   5. Hafta Ödevi-2
 
 #250 sayfalık bir kitap için 5 rakamı kaç kere kullanılmıştır...
+kelime = ""
+for i in range(1,251):
+  kelime +=str(i)
+print(kelime.count("5"))

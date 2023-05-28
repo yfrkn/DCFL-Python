f520ec4571d9b46d73f7d8fb382bd33f861224a7
Furkan <113787132+yfrkn@users.noreply.github.com>
2023-05-28 16:54:57 +0300

    Update Ödev-4.py

--- Ödevler/Ödev-4.py
+++ Ödevler/Ödev-4.py
@@ -1,1 +1,25 @@
+#     4. Ders Ödevi
 
+#koşul durmlarını kullanarak... mini bi hesap makinası yapınız!
+
+print("""
+Hesap Makinesi
+
+Toplama İşlemi - 1
+Çıkarma İşlemi - 2
+Çarpma İşlemi  - 3
+Bölme İşlemi   - 4
+""")
+islem = input("İşlem Numarası : ")
+s1 = int(input("1. Sayı = "))
+s2 = int(input("2. Sayı = "))
+if islem == "1":
+    print(s1, "+", s2, "=", s1+s2)
+elif islem == "2":
+    print(s1, "-", s2, "=", s1-s2)
+elif islem == "3":
+    print(s1, "*", s2, "=", s1*s2)
+elif islem == "4":
+    print(s1, "/", s2, "=", s1/s2)
+else:
+    print("Hatalı işlem girdiniz.")
